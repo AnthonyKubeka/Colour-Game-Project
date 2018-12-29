01b5bb7139cdb9c49134cc196986eb2550d500f3
Nkululeko Kubeka <nmkubeka@gmail.com>
2018-12-29 15:59:04 +0200

    Added Click event handlers, win condition logic

--- colourgame.js
+++ colourgame.js
@@ -5,25 +5,52 @@ const coloursList =[ "rgb(255, 0, 0)",
                     "rgb(0, 0, 255)",
                     "rgb(255, 0, 255)"];
 //the pickedColor is the colour we want to choose correctly
-var pickedColour = coloursList[3];
-function assignColours(){
-    //randomly assign colour options
-    coloursList.forEach((col, iterator)=>{
-       document.querySelectorAll('.square')[iterator].style.backgroundColor = col;
+var pickedColour = randomColPicker();
+var squares = document.querySelectorAll('.square');
+var messageDisplay = document.getElementById('message'); //displays 'correct' or 'try again' to the user
+
+  //show our pickedColour in the header
+document.getElementById('pickedCol').textContent = pickedColour.toUpperCase();
+
+
+coloursList.forEach((col, index)=>{
+        //randomly assign colour options
+    squares[index].style.backgroundColor = col;
     
+//---------Click Events Logic-------//
+      /*When we click on one of the squares we want to: 
+      get the square we clicked on
+      compare its colour to pickedColour
+      if they are different we grey out the square, and provide handling
+      if they are the same the player has won and we provide some handling
+      */
+    squares[index].addEventListener('click',()=>{
+        //grab the colour of the square that was clicked
+        var clickedColour  = squares[index].style.backgroundColor; //in a normal for loop (this.style would work)
+        if (clickedColour === pickedColour){
+            messageDisplay.textContent = 'Correct!';
+            changeColours(pickedColour);
+        }else{
+            squares[index].style.backgroundColor = '#232323';
+            messageDisplay.textContent = 'Try Again!';
+        }
+    });
     });
     
-    //show our pickedColour in the header
-    document.getElementById('pickedCol').textContent = pickedColour.toUpperCase();
-
+//The changeColours function changes all the squares to the correct colour when it is clicked by the user
+function changeColours(colour){
+    for (let i = 0; i<squares.length; i++){
+        squares[i].style.background = colour; 
+    }
 }
-assignColours();
 
-//---Click Events Logic----//
-/*When we click on one of the squares we want to: 
-    get the square we clicked on
-    compare its colour to pickedColour
-    if they are different we grey out the square, and provide handling
-    if they are the same the player has won and we provide some handling
-    */
+//The randomColPicker function will choose a random element of the coloursList array to be the picked / target colour 
+function randomColPicker(){//choose between 1 and the array length (3 or 6, for now)
+    var el = Math.floor(Math.random()*coloursList.length);
+    return coloursList[el];
+}
+  
+
+
+
 
